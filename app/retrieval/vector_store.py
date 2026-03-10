7213f27d7d03c70d601882e63b942e8ede44b220
mayankpahade611 <mayankpahade4@gmail.com>
2026-03-11 01:32:03 +0530

    add lru_cache decorator in vector_store file

--- app/retrieval/vector_store.py
+++ app/retrieval/vector_store.py
@@ -3,14 +3,18 @@ from qdrant_client.models import VectorParams, Distance
 from langchain_community.vectorstores import Qdrant
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from app.config import settings
+from functools import lru_cache
 
 COLLECTION_NAME = "research_docs"
 
-
-def get_vector_store():
-    embeddings = HuggingFaceEmbeddings(
+@lru_cache
+def get_embeddings():
+    return HuggingFaceEmbeddings(
         model_name="sentence-transformers/all-MiniLM-L6-v2"
     )
+
+def get_vector_store():
+    embeddings = get_embeddings()
 
     client = QdrantClient(
     url=settings.QDRANT_URL,
