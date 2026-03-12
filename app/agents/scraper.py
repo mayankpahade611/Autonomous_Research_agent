57f3668c2d64230e9458870733d31e69a6982143
mayankpahade611 <mayankpahade4@gmail.com>
2026-03-12 15:18:15 +0530

    optimize scrapper and reduce execution time

--- app/agents/scraper.py
+++ app/agents/scraper.py
@@ -7,15 +7,17 @@ async def fetch_page(session, url):
     try:
         async with session.get(
             url, 
-            timeout=aiohttp.ClientTimeout(total=5),  # 10s → 5s
+            timeout=aiohttp.ClientTimeout(total=3),  # 10s → 5s
             headers={"User-Agent": "Mozilla/5.0"}    # avoid blocks
         ) as response:
+            if response.status != 200:
+                return None 
             html = await response.text()
             soup = BeautifulSoup(html, "html.parser")
             for script in soup(["script", "style"]):
                 script.decompose()
             text = soup.get_text(separator=" ")
-            return {"url": url, "content": text[:3000]}  # 8000 → 3000
+            return {"url": url, "content": text[:2000]}  # 8000 → 3000
     except Exception:
         return None
 
